94792ce6be6eaa4fb98bf124b60e18fceae2cdf8
Genevieve Heald <gennaheald@outlook.com>
2019-10-30 18:31:34 +0000

    Updates

---
+++ precache-manifest.5901545a748a1d4a8bc4314c3b42f445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cc9050955465944d6e8b29d203299d",
+    "url": "/manana-react/index.html"
+  },
+  {
+    "revision": "4963afa3b9aa3616ca62",
+    "url": "/manana-react/static/css/main.151d25f6.chunk.css"
+  },
+  {
+    "revision": "0d2d4273047b911de933",
+    "url": "/manana-react/static/js/2.f9a209db.chunk.js"
+  },
+  {
+    "revision": "4963afa3b9aa3616ca62",
+    "url": "/manana-react/static/js/main.088c4836.chunk.js"
+  },
+  {
+    "revision": "bc255d08b4ee98517b30",
+    "url": "/manana-react/static/js/runtime-main.57862528.js"
+  }
+]);
